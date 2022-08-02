94a3f6960a07517d70efd148aafa76fca180eb59
Chris Ho <chris.tridung@gmail.com>
2022-08-02 18:04:05 +0700

    add firebase and sign-in/up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,45 @@
-import type { NextPage } from "next";
+import type { GetServerSidePropsContext, NextPage } from 'next';
+import { signIn, signOut, useSession, getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <button
+          className="bg-gray-200 rounded px-3 py-2"
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
+      </>
+    );
+  }
+
   return (
-    <h1 className="mt-10 heading uppercase text-center text-7xl">
-      The web3
-      <br />
-      <span className="text-decode3">Fundraising</span>
-      <br />
-      Database
-    </h1>
+    <>
+      Not signed in <br />
+      <button
+        className="bg-decode3 px-3 py-2 rounded text-white"
+        onClick={() => router.push('/signin')}
+      >
+        Sign in
+      </button>
+    </>
   );
 };
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session
+    }
+  };
+}
 
 export default Home;
---
+++ pages/signin.tsx
@@ -0,0 +1,78 @@
+import {
+  getProviders,
+  signIn,
+  getCsrfToken,
+  getSession
+} from 'next-auth/react';
+import { CommonProviderOptions } from 'next-auth/providers';
+import { GetServerSidePropsContext } from 'next';
+import GoogleIcon from '../components/icons/GoogleIcon';
+
+export default function SignIn({
+  providers,
+  csrfToken
+}: {
+  providers: Record<'google' | 'credentials', CommonProviderOptions>;
+  csrfToken: string;
+}) {
+  return (
+    <div className="max-w-5xl mx-auto flex items-center justify-center">
+      <div className="py-10 grid grid-cols-1 gap-y-10">
+        <h2 className="heading text-white text-center">LOG IN</h2>
+        <button
+          className="border border-white px-3 py-2 text-white flex items-center space-x-2"
+          onClick={() => signIn(providers?.google.id)}
+        >
+          <GoogleIcon className="w-6 h-6" />
+          <span>Sign in with Google</span>
+        </button>
+        <form method="post" action="/api/auth/callback/credentials">
+          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+          <div className="flex flex-col">
+            <label className="text-white">Username</label>
+            <input
+              className="py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300"
+              name="Email"
+              type="email"
+              required
+            />
+          </div>
+          <div className="flex flex-col mt-2">
+            <label className="text-white">Password</label>
+            <input
+              required
+              className="py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300"
+              name="password"
+              type="password"
+            />
+          </div>
+          <button className="mt-5 bg-decode3 w-full py-2 px-3" type="submit">
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context);
+  const providers = await getProviders();
+  if (session && context.res) {
+    context.res.statusCode = 302;
+    context.res.setHeader('Location', '/');
+
+    return {
+      props: { session, providers }
+    };
+  }
+
+  const csrfToken = await getCsrfToken(context);
+
+  return {
+    props: {
+      providers,
+      csrfToken
+    }
+  };
+}
