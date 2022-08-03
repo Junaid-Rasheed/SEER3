d9457a848dbe451131ade0facac3263d34447294
binhnd <tthandb@hotmail.com>
2022-08-03 08:33:53 +0700

    create ui #1

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image';
 import GetStartedButton from './GetStartedButton';
+import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export default function Header() {
+  const { data: session } = useSession();
+  const router = useRouter();
   return (
-    <nav className="grid grid-cols-3 py-4 bg-black border-b border-b-neutral-600">
+    <nav className="grid grid-cols-3 py-4 bg-black border-b border-b-neutral-600 relative">
       <a href="#" className="w-[180px] h-[40px] relative ml-8">
         <Image
           src="/assets/desktop/DECODE Material-08 landscape.png"
@@ -17,11 +21,24 @@ export default function Header() {
         <li className="text-white uppercase py-2 px-6"><a href="#">Contact</a></li>
       </ul>
       <div className="flex flex-row gap-9 mr-8 justify-end">
-        <button className="border border-decode3 text-decode3 font-bold py-2 px-6">
-          Log in
-        </button>
+        {!!session ?
+          <button
+            className="border border-decode3 text-decode3 font-bold py-2 px-6"
+            onClick={() => signOut()}
+          >
+            Log out
+          </button>
+          :
+          <button
+            className="border border-decode3 text-decode3 font-bold py-2 px-6"
+            onClick={() => router.push('/signin')}
+          >
+            Log in
+          </button>
+        }
         <GetStartedButton />
       </div>
+      {!!session && <h1 className="text-white absolute -bottom-10 right-5">{session?.user?.email}</h1>}
     </nav>
   );
 }
