cf8956b16a4ffb5007e9b25278543e8bde811c36
binhnd <tthandb@hotmail.com>
2022-08-08 18:36:22 +0700

    add animation to hero

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,10 +1,11 @@
 import { LoadingLogo } from './icons/Logos';
+import classNames from 'classnames';
 
-export default function Loading() {
+export default function Loading({ className }: { className?: string }) {
   return (
-    <div className="bg-black h-screen flex flex-col items-center justify-center">
+    <div className={classNames('bg-black h-full flex flex-col items-center justify-center', className)}>
       <LoadingLogo className="w-[373px] h-[323px]" />
-      <h1 className="heading text-white text-4xl">THE PITCHBOOK OF WEB3</h1>
+      <h1 className="mt-20 heading text-white text-4xl">THE PITCHBOOK OF WEB3</h1>
     </div>
   )
 }
