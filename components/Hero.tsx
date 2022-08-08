c13a9b8c8d0eb9db1bb018118230c7c2f2402846
binhnd <tthandb@hotmail.com>
2022-08-08 18:36:22 +0700

    add animation to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,24 +1,22 @@
 import GetStartedButton from './GetStartedButton';
-import { useEffect } from 'react';
-import { decodeAnimation } from '../utils/animation';
+import AnimatedText from './AnimatedText';
 
 export default function Hero() {
-  useEffect(() => {
-    decodeAnimation(document.getElementById('fundraising'));
-  }, [])
   return (
     <div className="relative flex justify-center items-center h-screen">
-      <video poster="/assets/desktop/desktop_thumbs.png" playsInline autoPlay muted loop className="absolute hidden md:block top-0 left-0 object-cover h-full w-screen">
+      <video poster="/assets/desktop/desktop_thumbs.png" playsInline autoPlay muted loop
+             className="absolute hidden md:block top-0 left-0 object-cover h-full w-screen">
         <source src="/assets/desktop/Horizontal.mp4" type="video/mp4" />
       </video>
-      <video poster="/assets/mobile/mobile_thumbs.png" playsInline autoPlay muted loop className="absolute md:hidden top-0 left-0 object-cover h-full w-screen">
+      <video poster="/assets/mobile/mobile_thumbs.png" playsInline autoPlay muted loop
+             className="absolute md:hidden top-0 left-0 object-cover h-full w-screen">
         <source src="/assets/mobile/Vertical.mp4" type="video/mp4" />
       </video>
       <div className="relative z-10 h-full flex flex-col items-center justify-center">
         <h1 className="mt-10 heading uppercase text-center text-5xl md:text-7xl text-white">
           The web3
           <br />
-          <span id="fundraising" className="text-decode3 text-6xl md:text-8xl">Fundraising</span>
+          <AnimatedText text="Fundraising" className="text-decode3 text-6xl md:text-8xl" timeEachLetter={50} />
           <br />
           Database
         </h1>
--- utils/animation.ts
+++ utils/animation.ts
@@ -1,38 +1,26 @@
-export function decodeAnimation(el: any, timeEachLetter = 100, customText = '') {
+export const randomText = (text: string, callback: (x: string) => void, timeEachLetter = 100) => {
   const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split('');
-  const start = ['1e4', '1t6', '1h0', '3fr', '3yu', '3vw', 'bc1', '0x1', '0x5', '0x7'];
-  const firstLetters = start[Math.floor(Math.random() * start.length)];
 
   function randomString(len: number, text: string, index: number) {
-    let string = firstLetters;
-    for (let i = 0; i < len; i++) {
-      if (i > 2) {
-        if (text[i] === ' ') {
-          string += ' ';
-        } else {
-          string += letters[Math.floor(Math.random() * letters.length)];
-        }
+    let string = '';
+    for (let i = 0; i < len; ++i) {
+      if (text[i] === ' ') {
+        string += ' ';
+      } else {
+        string += letters[Math.floor(Math.random() * letters.length)];
       }
     }
     return string.slice(index);
   }
 
-  function getText(len: number, text: string) {
-    let string = '';
-    for (let i = 0; i < len; i++) {
-      string += text[i];
-    }
-    return string;
-  }
-
   function constructText(text: string, index: number) {
     setTimeout(function () {
-      el.innerHTML = getText(index, text) + randomString(text.length, text, index);
+      const ans = text.slice(0, index) + randomString(text.length, text, index);
+      callback(ans)
     }, index * timeEachLetter);
   }
 
-  const text = customText || el.textContent;
-  for (let i = 0; i <= text.length; i++) {
+  for (let i = 0; i <= text.length; ++i) {
     constructText(text, i);
   }
 }
