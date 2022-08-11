c246464cdf060b6c88c2b3462351c67361d99be3
Chris Ho <chris.tridung@gmail.com>
2022-08-11 22:51:14 +0700

    install typeform lib and fix it

--- pages/careers.tsx
+++ pages/careers.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
+import { Widget } from '@typeform/embed-react';
 import Layout from '../components/Layout';
-
-const markdown = `
-  <div data-tf-widget="BBbUa9CW" data-tf-iframe-props="title=Decode 3 Careers" data-tf-medium="snippet" style="width:100%;height:100%;"></div>
-  <script src="//embed.typeform.com/next/embed.js"> </script>
-`;
 
 const Careers = () => {
   return (
     <Layout>
-      <div className="py-20 bg-black flex items-center" dangerouslySetInnerHTML={{ __html: markdown }} />
+      <Widget id="BBbUa9CW" style={{ width: '100%', height: '100%' }} />
     </Layout>
   );
 };
--- pages/list-your-startup.tsx
+++ pages/list-your-startup.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
+import { Widget } from '@typeform/embed-react';
 import Layout from '../components/Layout';
-
-const markdown = `
-  <div data-tf-widget="EFcNPa5H" data-tf-iframe-props="title=Decode 3 Startups" data-tf-medium="snippet" style="width:100%;height:100%;"></div>
-  <script src="//embed.typeform.com/next/embed.js"></script>
-`;
 
 const ListYourStartup = () => {
   return (
     <Layout>
-      <div className="py-20 bg-black flex items-center" dangerouslySetInnerHTML={{ __html: markdown }} />
+      <Widget id="EFcNPa5H" style={{ width: '100%', height: '100%' }} />
     </Layout>
   );
 };
