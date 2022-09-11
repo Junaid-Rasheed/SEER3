b11bddfa0cf21feb003beeb321658696ceee76c2
mak9606 <alikhan1996116@gmail.com>
2022-09-11 09:12:45 +0500

    dark light mode fixes

--- components/layouts/DashboardLayout.tsx
+++ components/layouts/DashboardLayout.tsx
@@ -1,12 +1,73 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useMemo, useState } from 'react';
+import { Global, css, ThemeProvider } from '@emotion/react';
+import {
+  createTheme,
+  colors,
+  CssBaseline,
+  responsiveFontSizes
+} from '@mui/material';
+
 import TopBar from '../dashboard/TopBar';
+import { ColorModeContext } from '../../service/colormode.service';
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  const [mode, setMode] = useState<'light' | 'dark'>('dark');
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode: string) =>
+          prevMode === 'light' ? 'dark' : 'light'
+        );
+      }
+    }),
+    []
+  );
+  const theme = useMemo(() => {
+    if (typeof window !== 'undefined') localStorage.setItem('themeMode', mode);
+    let theme = createTheme({
+      palette: {
+        mode,
+        primary: {
+          dark: colors.red[100],
+          light: colors.amber[100],
+          main: '#4D77FF',
+          contrastText: colors.lime[100]
+        }
+      }
+    });
+    theme = responsiveFontSizes(theme);
+    return theme;
+  }, [mode]);
+
+  const GlobalStyles = () => {
+    return (
+      <Global
+        styles={css`
+          body {
+            background-color: ${theme.palette.mode === 'light'
+              ? '#fff'
+              : '#000'};
+          }
+          html,
+          body,
+          #root {
+            height: 100vh;
+          }
+        `}
+      />
+    );
+  };
   return (
-    <>
-      <TopBar />
-      <div className="text-white">{children}</div>
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <CssBaseline />
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        {' '}
+        <TopBar />
+        <div className="text-white">{children}</div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,53 +25,53 @@ import { ColorModeContext } from '../service/colormode.service';
 const protectedRoutes = ['/dashboard'];
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const [mode, setMode] = useState<'light' | 'dark'>('dark');
+  // const [mode, setMode] = useState<'light' | 'dark'>('dark');
 
-  const colorMode = useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode: string) =>
-          prevMode === 'light' ? 'dark' : 'light'
-        );
-      }
-    }),
-    []
-  );
-  const theme = useMemo(() => {
-    if (typeof window !== 'undefined') localStorage.setItem('themeMode', mode);
-    let theme = createTheme({
-      palette: {
-        mode,
-        primary: {
-          dark: colors.red[100],
-          light: colors.amber[100],
-          main: '#4D77FF',
-          contrastText: colors.lime[100]
-        }
-      }
-    });
-    theme = responsiveFontSizes(theme);
-    return theme;
-  }, [mode]);
+  // const colorMode = useMemo(
+  //   () => ({
+  //     toggleColorMode: () => {
+  //       setMode((prevMode: string) =>
+  //         prevMode === 'light' ? 'dark' : 'light'
+  //       );
+  //     }
+  //   }),
+  //   []
+  // );
+  // const theme = useMemo(() => {
+  //   if (typeof window !== 'undefined') localStorage.setItem('themeMode', mode);
+  //   let theme = createTheme({
+  //     palette: {
+  //       mode,
+  //       primary: {
+  //         dark: colors.red[100],
+  //         light: colors.amber[100],
+  //         main: '#4D77FF',
+  //         contrastText: colors.lime[100]
+  //       }
+  //     }
+  //   });
+  //   theme = responsiveFontSizes(theme);
+  //   return theme;
+  // }, [mode]);
 
-  const GlobalStyles = () => {
-    return (
-      <Global
-        styles={css`
-          body {
-            background-color: ${theme.palette.mode === 'light'
-              ? '#fff'
-              : '#000'};
-          }
-          html,
-          body,
-          #root {
-            height: 100vh;
-          }
-        `}
-      />
-    );
-  };
+  // const GlobalStyles = () => {
+  //   return (
+  //     <Global
+  //       styles={css`
+  //         body {
+  //           background-color: ${theme.palette.mode === 'light'
+  //             ? '#fff'
+  //             : '#000'};
+  //         }
+  //         html,
+  //         body,
+  //         #root {
+  //           height: 100vh;
+  //         }
+  //       `}
+  //     />
+  //   );
+  // };
   useRoutingProgressBar();
   const [isVisible] = useShowLoadingScreen(3700);
   const router = useRouter();
@@ -87,15 +87,9 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
       {getLayout(
         protectedRoutes.includes(router.pathname) ? (
-          <ColorModeContext.Provider value={colorMode}>
-            <CssBaseline />
-            <GlobalStyles />
-            <ThemeProvider theme={theme}>
-              <ProtectedRoute>
-                <Component {...pageProps} />
-              </ProtectedRoute>
-            </ThemeProvider>
-          </ColorModeContext.Provider>
+          <ProtectedRoute>
+            <Component {...pageProps} />
+          </ProtectedRoute>
         ) : (
           <Component {...pageProps} />
         )
